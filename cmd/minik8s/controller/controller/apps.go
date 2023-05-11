f492240a30e9c74d15c36b07bfc9e004704a1318
aliceziyun <2225870865@qq.com>
2023-05-11 16:30:33 +0800

    modify

--- cmd/minik8s/controller/controller/apps.go
+++ cmd/minik8s/controller/controller/apps.go
@@ -1,22 +1,16 @@
 package controller
 
-import (
-	"Mini-K8s/pkg/controller/deployment"
-	"context"
-	"fmt"
-)
-
-func startDeploymentController(ctx context.Context, controllerContext ControllerContext) (controller.Interface, bool, error) {
-	dc, err := deployment.NewDeploymentController(
-		ctx,
-		controllerContext.InformerFactory.Apps().V1().Deployments(),
-		controllerContext.InformerFactory.Apps().V1().ReplicaSets(),
-		controllerContext.InformerFactory.Core().V1().Pods(),
-		controllerContext.ClientBuilder.ClientOrDie("deployment-controller"),
-	)
-	if err != nil {
-		return nil, true, fmt.Errorf("error creating Deployment controller: %v", err)
-	}
-	go dc.Run(ctx, int(controllerContext.ComponentConfig.DeploymentController.ConcurrentDeploymentSyncs))
-	return nil, true, nil
-}
+//func startDeploymentController(ctx context.Context, controllerContext ControllerContext) (controller.Interface, bool, error) {
+//	dc, err := deployment.NewDeploymentController(
+//		ctx,
+//		controllerContext.InformerFactory.Apps().V1().Deployments(),
+//		controllerContext.InformerFactory.Apps().V1().ReplicaSets(),
+//		controllerContext.InformerFactory.Core().V1().Pods(),
+//		controllerContext.ClientBuilder.ClientOrDie("deployment-controller"),
+//	)
+//	if err != nil {
+//		return nil, true, fmt.Errorf("error creating Deployment controller: %v", err)
+//	}
+//	go dc.Run(ctx, int(controllerContext.ComponentConfig.DeploymentController.ConcurrentDeploymentSyncs))
+//	return nil, true, nil
+//}
--- pkg/controller/deployment/deployment_controller.go
+++ pkg/controller/deployment/deployment_controller.go
@@ -1,20 +1,14 @@
 package deployment
 
-import (
-	"Mini-K8s/pkg/listwatcher"
-	"Mini-K8s/third_party/queue"
-	"time"
-)
-
 // DeploymentController :负责同步真实运行的Pod和存储在系统中期望的Pod数量
-type DeploymentController struct {
-	ls *listwatcher.ListWatcher //deployment informer + pod infomer
-
-	enqueueDeployment func(deployment *apps.Deployment)
-
-	resyncInterval time.Duration
-	stopChannel    chan struct{}
-	apiServerBase  string
-
-	queue queue.ConcurrentQueue //存储deployment的队列
-}
+//type DeploymentController struct {
+//	ls *listwatcher.ListWatcher //deployment informer + pod infomer
+//
+//	enqueueDeployment func(deployment *apps.Deployment)
+//
+//	resyncInterval time.Duration
+//	stopChannel    chan struct{}
+//	apiServerBase  string
+//
+//	queue queue.ConcurrentQueue //存储deployment的队列
+//}
